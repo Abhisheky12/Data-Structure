1c3a8be18ede4509279d4b1881f795045928117a
Abhisheky12 <166697064+Abhisheky12@users.noreply.github.com>
2025-01-18 20:13:26 +0530

    Add files via upload

---
+++ Linked List/combined questions/Remove duplicate from sorted linkedlist.cpp
@@ -0,0 +1,27 @@
+
+
+
+
+  // 2 2 2 3 3 4 4(linked list )
+class Solution {
+  public:
+    // Function to remove duplicates from sorted linked list.
+    Node* removeDuplicates(Node* head) {
+
+        // code here
+        Node *curr=head->next;
+        Node *prev=head;
+        while(curr!=NULL){
+            if(prev->data==curr->data){
+                prev->next=curr->next;
+                delete curr;
+                curr=prev->next;
+            }
+            else{
+                prev=prev->next;
+                curr=curr->next;
+            }
+        }
+        return head;
+    }
+};
