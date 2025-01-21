11fb48e4a58b7e5e5a86a585c6e7e41c16fbdbae
Abhisheky12 <166697064+Abhisheky12@users.noreply.github.com>
2025-01-21 23:54:10 +0530

    Add files via upload

---
+++ Linked List/circular linked l/detect and remove loop.cpp
@@ -0,0 +1,28 @@
+class Solution {
+  public:
+    // Function to remove a loop in the linked list.
+    void removeLoop(Node* head) {
+        // code here
+        Node *slow=head;
+        Node *fast=head;
+        while(fast!=NULL && fast->next!=NULL){
+            slow=slow->next;
+            fast=fast->next->next;
+            if(slow==fast){
+                break;
+            }
+        }
+        if(fast==NULL || fast->next==NULL){
+            return;
+        }
+        slow=head;
+        while(slow!=fast){
+            slow=slow->next;
+            fast=fast->next;
+        }
+        while(slow->next!=fast){
+            slow=slow->next;
+        }
+        slow->next=NULL;
+    }
+};
